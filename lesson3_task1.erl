928dbdfb321e7a464049fcc98770fa50725741a8
Oleskiy_ISDM52 <ut44ut@protonmail.com>
2024-01-03 19:51:45 +0200

    Improved task1 to support multi-byte utf8

--- lesson3_task1.erl
+++ lesson3_task1.erl
@@ -6,7 +6,17 @@ first_word(BinText) ->
 
 first_word(<<>>, Acc) ->
     Acc;
-first_word(<<32, _Rest/binary>>, Acc) ->
-    Acc;
-first_word(<<Char, Rest/binary>>, Acc) ->
-    first_word(Rest, <<Acc/binary, Char>>).
+first_word(BinText, Acc) ->
+    {Char, Rest} = next_utf8_char(BinText),
+    case Char of
+        <<32>> -> 
+            Acc;
+        _ ->
+            first_word(Rest, <<Acc/binary, Char/binary>>)
+    end.
+
+% Helper function for extracting the next UTF-8 character in binary format.
+next_utf8_char(<<Char1, Rest/binary>>) when Char1 band 128 =:= 0 ->   {<<Char1>>, Rest};
+next_utf8_char(<<Char1, Char2, Rest/binary>>) when Char1 band 224 =:= 192 ->  {<<Char1, Char2>>, Rest};
+next_utf8_char(<<Char1, Char2, Char3, Rest/binary>>) when Char1 band 240 =:= 224 ->   {<<Char1, Char2, Char3>>, Rest};
+next_utf8_char(<<Char1, Char2, Char3, Char4, Rest/binary>>) when Char1 band 248 =:= 240 ->  {<<Char1, Char2, Char3, Char4>>, Rest}.
